bae2fafd43b675a39078feae9545955ec11589ba
400219337 <400219337@B3F07050D1.ind.ge.ad>
2019-01-02 16:06:24 +0530

    hvujhv

--- src/main/java/com/genpact/model/Role.java
+++ src/main/java/com/genpact/model/Role.java
@@ -15,7 +15,6 @@ import org.hibernate.annotations.ManyToAny;
 @Entity
 @Table(name = "role")
 public class Role {
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "role_id")
